41e05dc3a9a1fcdbac7cbde2383ab0b671b82d01
root <root@fullcalendar-achim>
2020-09-03 16:23:08 +0200

    weitere features

--- src/collective/fullcalendar/behaviors/fullcalendar.py
+++ src/collective/fullcalendar/behaviors/fullcalendar.py
@@ -11,8 +11,8 @@ from Products.Five.browser import BrowserView
 from z3c.relationfield.schema import RelationChoice
 from zope.component import adapter
 from zope.interface import implementer, Interface, provider
+from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
-
 
 class IFullcalendarMarker(Interface):
     pass
@@ -22,67 +22,74 @@ class IFullcalendarMarker(Interface):
 class IFullcalendar(model.Schema):
     """
     """
+    model.fieldset(
+        'fullcalendar',
+        label=_(u"Fullcalendar"),
+        fields=['slotMinutes', 'allDay','defaultCalendarView','headerLeft','headerRight','weekends','firstDay','firstHour','minTime','maxTime','target_folder','calendarHeight','caleditable']
+    )
+
     slotMinutes = schema.Int(
         title=_(u'Slot length'),
         description=_(u'Slot length in minutes'),
-        required=False,
+        required=True,
+        default=30
     )
     allDay = schema.Bool(
-        title=_(u'Full day'),
-        description=_(u'Display \"All day\" option'),
+        title=_(u'All day'),
+        description=_(u'Display \"All day\" option for timeGridWeek-View'),
         required=False,
+        default=True
     )
-    #views_items = [
-    #    ('day', u'Day'),
-    #    ('week', u'Week'),
-    #    ('month', u'Month'),
-    #]
-    #views_terms = [SimpleTerm(value=pair[0], token=pair[0], title=pair[1]) for pair in views_items]
-    #views_vocabulary = SimpleVocabulary(views_terms)
-    #views_vocabulary = SimpleVocabulary.fromValues(['day','week','month'])
     defaultCalendarView = schema.Choice(
         title=_(u'Standard-Ansicht'),
         description=_(u'Standard-Ansicht'),
-        required=False,
-        vocabulary=SimpleVocabulary.fromValues(['day','week','month'])
+        required=True,
+        vocabulary=SimpleVocabulary.fromValues(['dayGridMonth','timeGridWeek','listWeek','dayGridWeek']),
+        default='dayGridMonth'
     )
-    headerLeft = schema.List(
+    # Possible for headerLeft/headerRight: title, prev, next, prevYear, nextYear, today, dayGridMonth, timeGridWeek, listWeek, dayGridWeek
+    headerLeft = schema.TextLine(
         title=_(u'Kopfbereich links'),
-        description=_(u'Kopfbereich links'),
+        description=_(u'Mögliche Werte: title, prev, next, prevYear, nextYear, today, dayGridMonth, timeGridWeek, listWeek, dayGridWeek'),
         required=False,
-        value_type=schema.Choice(vocabulary=SimpleVocabulary.fromValues(['prev','next']))
+        default = 'prev,next today'
     )
-    headerRight = schema.List(
+    headerRight = schema.TextLine(
         title=_(u'Kopfbereich rechts'),
-        description=_(u'Kopfbereich rechts'),
+        description=_(u'Mögliche Werte: title, prev, next, prevYear, nextYear, today, dayGridMonth, timeGridWeek, listWeek, dayGridWeek'),
         required=False,
-        value_type=schema.Choice(vocabulary=SimpleVocabulary.fromValues(['prev','next']))
+        default = 'dayGridMonth timeGridWeek listWeek'
     )
     weekends = schema.Bool(
         title=_(u'Wochenenden anzeigen'),
         description=_(u'Wochenenden anzeigen'),
         required=False,
+        default=True
     )
     firstDay = schema.Choice(
         title=_(u'Erster Wochentag'),
         description=_(u'Erster Wochentag'),
-        required=False,
-        vocabulary=SimpleVocabulary.fromValues(['monday','tuesday','wednesday','thursday','friday','saturday','sunday'])
+        required=True,
+        vocabulary=SimpleVocabulary([SimpleTerm(value=pair[0],token=pair[0],title=pair[1]) for pair in [(0,u'sunday'),(1,u'monday'),(2,u'tuesday'),(3,u'wednesday'),(4,u'thursday'),(5,u'friday'),(6,u'saturday')]]),
+        default=1
     )
-    firstDay = schema.TextLine(
+    firstHour = schema.TextLine(
         title=_(u'Erste angezeigte Stunde'),
-        description=_(u'Erste angezeigte Stunde'),
-        required=False,
+        description=_(u'Legen Sie die anfängliche Scrollposition der Kalender-Tagesansicht fest (eine Zahl zwischen 0 und 23). Wenn vor dieser Zahl ein "+" oder "-" steht, wird die Zahl mit der aktuellen Zeit addiert bzw. substrahiert.'),
+        required=True,
+        default='6'
     )
     minTime = schema.TextLine(
         title=_(u'Erste sichtbare Stunde'),
-        description=_(u'Erste sichtbare Stunde'),
-        required=False,
+        description=_(u'Wählen Sie die erste sichtbare Stunde des Kalenders (z.B. \'5\' oder \'5:30\').'),
+        required=True,
+        default='0'
     )
     maxTime = schema.TextLine(
         title=_(u'Letzte sichtbare Stunde'),
-        description=_(u'Letzte sichtbare Stunde'),
-        required=False,
+        description=_(u'Wählen Sie die letzte sichtbare Stunde des Kalenders (z.B. \'5\' oder \'5:30\').'),
+        required=True,
+        default='24'
     )
     # Zielordner für neue Termine
     target_folder = RelationChoice(
@@ -120,11 +127,119 @@ class Fullcalendar(object):
         self.context = context
 
     @property
-    def project(self):
-        if safe_hasattr(self.context, 'project'):
-            return self.context.project
+    def slotMinutes(self):
+        if safe_hasattr(self.context, 'slotMinutes'):
+            return self.context.slotMinutes
         return None
+    @slotMinutes.setter
+    def slotMinutes(self, value):
+        self.context.slotMinutes = value
 
-    @project.setter
-    def project(self, value):
-        self.context.project = value
+    @property
+    def allDay(self):
+        if safe_hasattr(self.context, 'allDay'):
+            return self.context.allDay
+        return None
+    @slotMinutes.setter
+    def allDay(self, value):
+        self.context.allDay = value
+
+    @property
+    def defaultCalendarView(self):
+        if safe_hasattr(self.context, 'defaultCalendarView'):
+            return self.context.defaultCalendarView
+        return None
+    @defaultCalendarView.setter
+    def defaultCalendarView(self, value):
+        self.context.defaultCalendarView = value
+
+    @property
+    def headerLeft(self):
+        if safe_hasattr(self.context, 'headerLeft'):
+            return self.context.headerLeft
+        return None
+    @headerLeft.setter
+    def headerLeft(self, value):
+        self.context.headerLeft = value
+
+    @property
+    def headerRight(self):
+        if safe_hasattr(self.context, 'headerRight'):
+            return self.context.headerRight
+        return None
+    @headerRight.setter
+    def headerRight(self, value):
+        self.context.headerRight = value
+
+    @property
+    def weekends(self):
+        if safe_hasattr(self.context, 'weekends'):
+            return self.context.weekends
+        return None
+    @weekends.setter
+    def weekends(self, value):
+        self.context.weekends = value
+
+    @property
+    def firstDay(self):
+        if safe_hasattr(self.context, 'firstDay'):
+            return self.context.firstDay
+        return None
+    @firstDay.setter
+    def firstDay(self, value):
+        self.context.firstDay = value
+
+    @property
+    def firstHour(self):
+        if safe_hasattr(self.context, 'firstHour'):
+            return self.context.firstHour
+        return None
+    @firstHour.setter
+    def firstHour(self, value):
+        self.context.firstHour = value
+
+    @property
+    def minTime(self):
+        if safe_hasattr(self.context, 'minTime'):
+            return self.context.minTime
+        return None
+    @minTime.setter
+    def minTime(self, value):
+        self.context.minTime = value
+
+    @property
+    def maxTime(self):
+        if safe_hasattr(self.context, 'maxTime'):
+            return self.context.maxTime
+        return None
+    @maxTime.setter
+    def maxTime(self, value):
+        self.context.maxTime = value
+
+    @property
+    def target_folder(self):
+        if safe_hasattr(self.context, 'target_folder'):
+            return self.context.target_folder
+        return None
+    @target_folder.setter
+    def target_folder(self, value):
+        self.context.target_folder = value
+
+    @property
+    def calendarHeight(self):
+        if safe_hasattr(self.context, 'calendarHeight'):
+            return self.context.calendarHeight
+        return None
+    @calendarHeight.setter
+    def calendarHeight(self, value):
+        self.context.calendarHeight = value
+
+    @property
+    def caleditable(self):
+        if safe_hasattr(self.context, 'caleditable'):
+            return self.context.caleditable
+        return None
+    @caleditable.setter
+    def caleditable(self, value):
+        self.context.caleditable = value
+
--- src/collective/fullcalendar/views/fullcalendar_view.py
+++ src/collective/fullcalendar/views/fullcalendar_view.py
@@ -2,10 +2,10 @@
 
 from collective.fullcalendar import _
 from Products.Five.browser import BrowserView
-
+from plone import api
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
+from DateTime import DateTime
 
 class FullcalendarView(BrowserView):
     # If you want to define a template here, please remove the template from
@@ -14,5 +14,112 @@ class FullcalendarView(BrowserView):
 
     def __call__(self):
         # Implement your own actions:
-        self.msg = _(u'A small message')
         return self.index()
+
+    def getEvents(self):
+        events = self.context.results()
+        results = []
+        for event in events:
+            obj = event.getObject()
+            result = {}
+            result['uid'] = obj.UID()
+            result['title'] = obj.Title()
+            result['start'] = obj.start
+            result['end'] = obj.end
+            result['url'] = obj.absolute_url()
+            results.append(result)
+        return results
+
+    def renderEvents(self):
+        events = self.getEvents()
+        caleditable = self.context.caleditable
+        result = ''
+        result = result+'[\n'
+        for event in events:
+            result = result+'{\n'
+            result = result+'  id: \''+event['uid']+'\',\n'
+            result = result+'  title: \''+event['title']+'\',\n'
+            result = result+'  start: \''+str(event['start'])+'\',\n'
+            result = result+'  end: \''+str(event['end'])+'\',\n'
+            if not caleditable:
+                result = result+'  url: \''+event['url']+'\'\n'
+            result = result+'},\n'
+        result = result+']\n'
+        return result
+
+    def getFirstDay(self):
+        firstDay = self.context.firstDay
+        return firstDay
+
+    def getSlotMinutes(self):
+        slotMinutes = self.context.slotMinutes
+        if slotMinutes<1:
+            result = '00:01:00'
+        elif slotMinutes<10:
+            result = '00:0'+str(slotMinutes)+':00'
+        elif slotMinutes<60:
+            result = '00:'+str(slotMinutes)+':00'
+        else:
+            result = '01:00:00'
+        return result
+
+    def getAllDay(self):
+        if self.context.allDay:
+            return 'true'
+        else:
+            return 'false'
+
+    def getWeekends(self):
+        if self.context.weekends:
+            return 'true'
+        else:
+            return 'false'
+
+    def getFirstHour(self):
+        firstHour = self.context.firstHour
+        firstHourInt = int(firstHour)
+        if '+' in firstHour or '-' in firstHour: # relative to now
+            now = DateTime()
+            time = now+firstHourInt/24
+            hour = time.hour()
+            result = str(hour)+':00:00'
+            if hour<10:
+                result = '0'+result
+        else:
+            if firstHourInt <10:
+                result = '0'+str(firstHour)+':00:00'
+            else:
+                if firstHourInt >23:
+                    firstHourInt = 23
+                result = str(firstHourInt)+':00:00'
+        return result
+
+    def getTime(self,time):
+        if time.isdigit(): # Volle Stunde
+            timeInt = int(time)
+            if timeInt<10:
+                result = '0'+time+':00'
+            else:
+                result = time+':00'
+        else: # Krumme Angabe, z.B. '5:30'
+            if len(time) == 4:
+                result = '0'+time
+            else:
+                result = time
+        return result
+
+    def getMinTime(self):
+        minTime = self.getTime(self.context.minTime)
+        return minTime
+
+    def getMaxTime(self):
+        maxTime = self.getTime(self.context.maxTime)
+        return maxTime
+
+    def getEditable(self):
+        if self.context.caleditable:
+            return 'true'
+        else:
+            return 'false'
+
+
