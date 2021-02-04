0c07f58e8688d0e13e5abbd015627097634552e8
Janina <hard@werkbank.de>
2021-02-04 14:09:12 +0100

    Changes to access views and data through adapter

--- src/collective/fullcalendar/views/fullcalendar_view.py
+++ src/collective/fullcalendar/views/fullcalendar_view.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from collective.fullcalendar import _
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from DateTime import DateTime
 from plone import api
 from Products.Five.browser import BrowserView
@@ -10,20 +9,15 @@ from plone.app.contenttypes.content import Folder
 
 
 class FullcalendarView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('fullcalendar_view.pt')
-
     def __call__(self):
-        # Implement your own actions:
         return self.index()
 
     def getEvents(self):
         typ = type(self.context.aq_base)
         if typ == Collection:
-          events = self.context.results()
+            events = self.context.results()
         elif typ == Folder:
-          events = api.content.find(context=self.context,portal_type='Event')
+            events = api.content.find(context=self.context, portal_type='Event')
         results = []
         for event in events:
             obj = event.getObject()
@@ -59,11 +53,11 @@ class FullcalendarView(BrowserView):
 
     def getSlotMinutes(self):
         slotMinutes = self.context.slotMinutes
-        if slotMinutes<1:
+        if slotMinutes < 1:
             result = '00:01:00'
-        elif slotMinutes<10:
+        elif slotMinutes < 10:
             result = '00:0'+str(slotMinutes)+':00'
-        elif slotMinutes<60:
+        elif slotMinutes < 60:
             result = '00:'+str(slotMinutes)+':00'
         else:
             result = '01:00:00'
@@ -84,30 +78,30 @@ class FullcalendarView(BrowserView):
     def getFirstHour(self):
         firstHour = self.context.firstHour
         firstHourInt = int(firstHour)
-        if '+' in firstHour or '-' in firstHour: # relative to now
+        if '+' in firstHour or '-' in firstHour:  # relative to now
             now = DateTime()
             time = now+firstHourInt/24
             hour = time.hour()
             result = str(hour)+':00:00'
-            if hour<10:
+            if hour < 10:
                 result = '0'+result
         else:
-            if firstHourInt <10:
+            if firstHourInt < 10:
                 result = '0'+str(firstHour)+':00:00'
             else:
-                if firstHourInt >23:
+                if firstHourInt > 23:
                     firstHourInt = 23
                 result = str(firstHourInt)+':00:00'
         return result
 
-    def getTime(self,time):
-        if time.isdigit(): # Volle Stunde
+    def getTime(self, time):
+        if time.isdigit():  # Volle Stunde
             timeInt = int(time)
-            if timeInt<10:
+            if timeInt < 10:
                 result = '0'+time+':00'
             else:
                 result = time+':00'
-        else: # Krumme Angabe, z.B. '5:30'
+        else:  # Krumme Angabe, z.B. '5:30'
             if len(time) == 4:
                 result = '0'+time
             else:
