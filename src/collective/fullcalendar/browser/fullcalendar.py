5609f7961bded97a5e66b8a11c1f122c36ba0b10
Janina <hard@werkbank.de>
2021-03-11 18:14:44 +0100

    add locales for settings

--- src/collective/fullcalendar/browser/fullcalendar.py
+++ src/collective/fullcalendar/browser/fullcalendar.py
@@ -24,72 +24,137 @@ from plone.app.z3cform.widget import RelatedItemsFieldWidget
 class IIFullcalenderSettings(Interface):
 
     slotMinutes = schema.Int(
-        title=_(u'Slot length'),
-        description=_(u'Slot length in minutes'),
+        title=_(
+            u'label_slot_length',
+            default=u'Slot length'),
+        description=_(
+            u'help_slot_length',
+            default=u'Slot length in minutes'
+        ),
         required=True,
         default=30
     )
     allDay = schema.Bool(
-        title=_(u'All day'),
-        description=_(u'Display \"All day\" option for timeGridWeek-View'),
+        title=_(
+            u'label_allDay',
+            default=u'All day'
+        ),
+        description=_(
+            u'help_allDay',
+            default=u'Display \"All day\" option for timeGridWeek-View'
+        ),
         required=False,
         default=True
     )
     defaultCalendarView = schema.Choice(
-        title=_(u'Standard-Ansicht'),
-        description=_(u'Standard-Ansicht'),
+        title=_(
+            u'label_defaultCalendarView',
+            default=u'Standard View'
+        ),
+        description=_(
+            u'help_defaultCalendarView',
+            default=u'Standard View'
+        ),
         required=True,
         vocabulary=SimpleVocabulary.fromValues(['dayGridMonth', 'timeGridWeek', 'listWeek', 'dayGridWeek']),
         default='dayGridMonth'
     )
     # Possible for headerLeft/headerRight: title, prev, next, prevYear, nextYear, today, dayGridMonth, timeGridWeek, listWeek, dayGridWeek
     headerLeft = schema.TextLine(
-        title=_(u'Kopfbereich links'),
-        description=_(u'Mögliche Werte: title, prev, next, prevYear, nextYear, today, dayGridMonth, timeGridWeek, listWeek, dayGridWeek'),
+        title=_(
+            u'label_headerLeft',
+            default=u'Kopfbereich links'
+        ),
+        description=_(
+            u'help_headerLeft',
+            default=u'Mögliche Werte: title, prev, next, prevYear, nextYear, today, dayGridMonth, timeGridWeek, listWeek, dayGridWeek'
+        ),
         required=False,
         default='prev,next today'
     )
     headerRight = schema.TextLine(
-        title=_(u'Kopfbereich rechts'),
-        description=_(u'Mögliche Werte: title, prev, next, prevYear, nextYear, today, dayGridMonth, timeGridWeek, listWeek, dayGridWeek'),
+        title=_(
+            u'label_headerRight',
+            default=u'Kopfbereich rechts'
+        ),
+        description=_(
+            u'help_headerRight',
+            default=u'Mögliche Werte: title, prev, next, prevYear, nextYear, today, dayGridMonth, timeGridWeek, listWeek, dayGridWeek'
+        ),
         required=False,
         default='dayGridMonth timeGridWeek listWeek'
     )
     weekends = schema.Bool(
-        title=_(u'Wochenenden anzeigen'),
-        description=_(u'Wochenenden anzeigen'),
+        title=_(
+            u'label_weekends',
+            default=u'Wochenenden anzeigen'
+        ),
+        description=_(
+            u'help_weekends',
+            default=u'Wochenenden anzeigen'
+        ),
         required=False,
         default=True
     )
     firstDay = schema.Choice(
-        title=_(u'Erster Wochentag'),
-        description=_(u'Erster Wochentag'),
+        title=_(
+            u'label_firstDay',
+            default=u'Erster Wochentag'
+        ),
+        description=_(
+            u'help_firstDay',
+            default=u'Erster Wochentag'
+        ),
         required=True,
         vocabulary=SimpleVocabulary([SimpleTerm(value=pair[0], token=pair[0], title=pair[1]) for pair in [(0, u'sunday'), (1, u'monday'), (2, u'tuesday'), (3, u'wednesday'), (4, u'thursday'), (5, u'friday'), (6, u'saturday')]]),
         default=1
     )
     firstHour = schema.TextLine(
-        title=_(u'Erste angezeigte Stunde'),
-        description=_(u'Legen Sie die anfängliche Scrollposition der Kalender-Tagesansicht fest (eine Zahl zwischen 0 und 23). Wenn vor dieser Zahl ein "+" oder "-" steht, wird die Zahl mit der aktuellen Zeit addiert bzw. substrahiert.'),
+        title=_(
+            u'label_firstHour',
+            default=u'Erste angezeigte Stunde'
+        ),
+        description=_(
+            u'help_firstHour',
+            default=u'Legen Sie die anfängliche Scrollposition der Kalender-Tagesansicht fest (eine Zahl zwischen 0 und 23). Wenn vor dieser Zahl ein "+" oder "-" steht, wird die Zahl mit der aktuellen Zeit addiert bzw. substrahiert.'
+        ),
         required=True,
         default='6'
     )
     minTime = schema.TextLine(
-        title=_(u'Erste sichtbare Stunde'),
-        description=_(u'Wählen Sie die erste sichtbare Stunde des Kalenders (z.B. \'5\' oder \'5:30\').'),
+        title=_(
+            u'label_minTime',
+            default=u'Erste sichtbare Stunde'
+        ),
+        description=_(
+            u'help_minTime',
+            default=u'Wählen Sie die erste sichtbare Stunde des Kalenders (z.B. \'5\' oder \'5:30\').'
+        ),
         required=True,
         default='0'
     )
     maxTime = schema.TextLine(
-        title=_(u'Letzte sichtbare Stunde'),
-        description=_(u'Wählen Sie die letzte sichtbare Stunde des Kalenders (z.B. \'5\' oder \'5:30\').'),
+        title=_(
+            u'label_maxTime',
+            default=u'Letzte sichtbare Stunde'
+        ),
+        description=_(
+            u'help_maxTime',
+            default=u'Wählen Sie die letzte sichtbare Stunde des Kalenders (z.B. \'5\' oder \'5:30\').'
+        ),
         required=True,
         default='24'
     )
     # Zielordner für neue Termine
     target_folder = RelationChoice(
-        title=u"Zielordner für neue Termine",
-        description=_(u'Zielordner für neue Termine'),
+        title=_(
+            u'label_target_folder',
+            default=u'Zielordner für neue Termine'
+        ),
+        description=_(
+            u'help_target_folder',
+            default=u'Zielordner für neue Termine'
+        ),
         vocabulary='plone.app.vocabularies.Catalog',
         required=False,
     )
@@ -103,14 +168,26 @@ class IIFullcalenderSettings(Interface):
     )
     # Höhe des Kalenders
     calendarHeight = schema.Int(
-        title=_(u'Calendar height'),
-        description=_(u'Calendar height in pixels'),
+        title=_(
+            u'label_calendarHeight',
+            default=u'Calendar height'
+        ),
+        description=_(
+            u'help_calendarHeight',
+            default=u'Calendar height in pixels'
+        ),
         required=False,
     )
     # Bearbeitung von Terminen erlauben
     caleditable = schema.Bool(
-        title=_(u'Calendar editable'),
-        description=_(u'Check this box if you want the events in the calendar to be editable.'),
+        title=_(
+            u'label_caleditable',
+            default=u'Calendar editable'
+        ),
+        description=_(
+            u'help_caleditable',
+            default=u'Check this box if you want the events in the calendar to be editable.'
+        ),
         required=False,
     )
 
--- src/collective/fullcalendar/views/edit_event.py
+++ src/collective/fullcalendar/views/edit_event.py
@@ -6,13 +6,8 @@ from plone import api
 from datetime import datetime
 import transaction
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
 
 class EditEvent(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('edit_event.pt')
 
     def __call__(self):
         return self.index()
@@ -20,47 +15,46 @@ class EditEvent(BrowserView):
     def process(self):
         form = self.request.form
         result = {}
-        if not 'uid' in form.keys():
-          result['status'] = 'ERROR'
-          result['msg'] = 'Keine UID übergeben.'
-          return result
+        if 'uid' not in form.keys():
+            result['status'] = 'ERROR'
+            result['msg'] = 'Keine UID übergeben.'
+            return result
         uid = form['uid']
         brains = api.content.find(UID=uid)
         if not brains:
-          result['status'] = 'ERROR'
-          result['msg'] = 'Kein Termin mit dieser UID gefunden.'
-          return result
+            result['status'] = 'ERROR'
+            result['msg'] = 'Kein Termin mit dieser UID gefunden.'
+            return result
         obj = brains[0].getObject()
-        if not 'method' in form.keys():
-          result['status'] = 'ERROR'
-          result['msg'] = 'Keine Methode (resize oder move) ausgewählt.'
-          return result
+        if 'method' not in form.keys():
+            result['status'] = 'ERROR'
+            result['msg'] = 'Keine Methode (resize oder move) ausgewählt.'
+            return result
         method = form['method']
-        if not method in ['resize','move']:
-          result['status'] = 'ERROR'
-          result['msg'] = 'Methode ist weder resize noch move.'
-          return result
-        if not 'new_end' in form.keys():
-          result['status'] = 'ERROR'
-          result['msg'] = 'Kein neuer Endzeitpunkt des Termins übergeben.'
-          return result
+        if method not in ['resize', 'move']:
+            result['status'] = 'ERROR'
+            result['msg'] = 'Methode ist weder resize noch move.'
+            return result
+        if 'new_end' not in form.keys():
+            result['status'] = 'ERROR'
+            result['msg'] = 'Kein neuer Endzeitpunkt des Termins übergeben.'
+            return result
         new_end_form = form['new_end']
         try:
-            new_end = datetime.strptime(new_end_form,'%Y-%m-%dT%H:%M:%S.%fZ')
+            new_end = datetime.strptime(new_end_form, '%Y-%m-%dT%H:%M:%S.%fZ')
         except ValueError as e:
             result['status'] = 'ERROR'
             result['msg'] = 'Datumsformat des neuen Endzeitpunkt des Termins unbekannt.'
             return result
         if method == 'move':
-          new_start_form = form['new_start']
-          try:
-              new_start = datetime.strptime(new_start_form,'%Y-%m-%dT%H:%M:%S.%fZ')
-          except ValueError as e:
-            result['status'] = 'ERROR'
-            result['msg'] = 'Datumsformat des neuen Startzeitpunkt des Termins unbekannt.'
-            return result
-          obj.start = new_start
+            new_start_form = form['new_start']
+            try:
+                new_start = datetime.strptime(new_start_form, '%Y-%m-%dT%H:%M:%S.%fZ')
+            except ValueError as e:
+                result['status'] = 'ERROR'
+                result['msg'] = 'Datumsformat des neuen Startzeitpunkt des Termins unbekannt.'
+                return result
+            obj.start = new_start
         obj.end = new_end
         transaction.commit()
         self.request.response.redirect('./@@fullcalendar-view')
-
