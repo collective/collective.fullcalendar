c928771fcfdc6d5474e67946c3edd608cc4a6edf
ksuess <k.suess@rohberg.ch>
2021-10-18 07:14:26 +0200

    black and renaming calender to calendar

--- src/collective/fullcalendar/views/edit_event.py
+++ src/collective/fullcalendar/views/edit_event.py
@@ -3,63 +3,63 @@
 from collective.fullcalendar import _
 from Products.Five.browser import BrowserView
 from plone import api
-from datetime import datetime
 from dateutil.parser import parse
 import transaction
 
 
 class EditEvent(BrowserView):
-
     def __call__(self):
         return self.index()
 
     def process(self):
         form = self.request.form
         result = {}
-        if 'uid' not in form.keys():
-            result['status'] = 'ERROR'
-            result['msg'] = 'Keine UID übergeben.'
+        if "uid" not in form.keys():
+            result["status"] = "ERROR"
+            result["msg"] = "Keine UID übergeben."
             return result
-        uid = form['uid']
+        uid = form["uid"]
         brains = api.content.find(UID=uid)
         if not brains:
-            result['status'] = 'ERROR'
-            result['msg'] = 'Kein Termin mit dieser UID gefunden.'
+            result["status"] = "ERROR"
+            result["msg"] = "Kein Termin mit dieser UID gefunden."
             return result
         obj = brains[0].getObject()
-        if 'method' not in form.keys():
-            result['status'] = 'ERROR'
-            result['msg'] = 'Keine Methode (resize oder move) ausgewählt.'
+        if "method" not in form.keys():
+            result["status"] = "ERROR"
+            result["msg"] = "Keine Methode (resize oder move) ausgewählt."
             return result
-        method = form['method']
-        if method not in ['resize', 'move']:
-            result['status'] = 'ERROR'
-            result['msg'] = 'Methode ist weder resize noch move.'
+        method = form["method"]
+        if method not in ["resize", "move"]:
+            result["status"] = "ERROR"
+            result["msg"] = "Methode ist weder resize noch move."
             return result
-        if 'new_end' not in form.keys():
-            result['status'] = 'ERROR'
-            result['msg'] = 'Kein neuer Endzeitpunkt des Termins übergeben.'
+        if "new_end" not in form.keys():
+            result["status"] = "ERROR"
+            result["msg"] = "Kein neuer Endzeitpunkt des Termins übergeben."
             return result
-        new_end_form = form['new_end']
+        new_end_form = form["new_end"]
         try:
             # new_end = datetime.strptime(new_end_form, '%Y-%m-%dT%H:%M:%S.%f')
             new_end = self.parse_date(new_end_form)
         except ValueError as e:
-            result['status'] = 'ERROR'
-            result['msg'] = 'Datumsformat des neuen Endzeitpunkt des Termins unbekannt.'
+            result["status"] = "ERROR"
+            result["msg"] = "Datumsformat des neuen Endzeitpunkt des Termins unbekannt."
             return result
-        if method == 'move':
-            new_start_form = form['new_start']
+        if method == "move":
+            new_start_form = form["new_start"]
             try:
                 new_start = self.parse_date(new_start_form)
             except ValueError as e:
-                result['status'] = 'ERROR'
-                result['msg'] = 'Datumsformat des neuen Startzeitpunkt des Termins unbekannt.'
+                result["status"] = "ERROR"
+                result[
+                    "msg"
+                ] = "Datumsformat des neuen Startzeitpunkt des Termins unbekannt."
                 return result
             obj.start = new_start
         obj.end = new_end
         transaction.commit()
-        self.request.response.redirect(self.request['HTTP_REFERER'])
+        self.request.response.redirect(self.request["HTTP_REFERER"])
 
     def parse_date(self, date_str):
         dt = parse(date_str)
