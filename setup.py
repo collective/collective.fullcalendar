a7a604844611addbb4c1cd5216337fd01fe5255c
Katja Süss <k.suess@rohberg.ch>
2024-12-06 14:28:44 +0100

    Back to development: 1.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.fullcalendar",
-    version="1.4",
+    version="1.5.dev0",
     description="Fullcalendar for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
